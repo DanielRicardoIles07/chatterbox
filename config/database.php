21136eaca56a39a9cc5cf8533d90b44be020f809
Danie Ricardo Iles <danieliles18@gmail.com>
2017-10-25 16:15:16 -0500

    upload project with login

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-54-204-41-80.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd65869gj5ktin7'),
+            'username' => env('DB_USERNAME', 'uzbytoonitbqhv'),
+            'password' => env('DB_PASSWORD', 'd66ed451541e53df342400f8549fc1803507d4ed446bcde26a3f847d8ffc8c1d'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
