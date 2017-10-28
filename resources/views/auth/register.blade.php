c03ef8e06b5257c1ff034fd4245f33f138e1cf6e
Danie Ricardo Iles <danieliles18@gmail.com>
2017-10-28 09:56:16 -0500

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -28,7 +28,7 @@
           <header class="masthead clearfix">
             <div class="inner">
               <nav class="nav nav-masthead">
-                <a class="nav-link " href="{{ url('/welcome') }}">Iniciar Sesion</a>
+                <a class="nav-link " href="{{ url('/') }}">Iniciar Sesion</a>
                 <a class="nav-link active" href="{{ route('register') }}">Registrarme</a>
               </nav>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('auth/register');
-});
-Route::get('/welcome', function () {
     return view('welcome');
 });
+
 
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
